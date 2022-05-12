f6504a5e41dc3ba0eaeda29aa78e39851dbd7d04
Oskar505 <82381961+Oskar505@users.noreply.github.com>
2022-05-12 21:02:12 +0200

    Add files via upload

---
+++ ezJson.py
@@ -0,0 +1,53 @@
+import json
+
+
+class EzJson:
+    
+    def __init__(self, fileName):
+        self.fileName = fileName
+
+        self.f = open(self.fileName)
+        self.jsonData = json.load(self.f)
+        
+
+
+    def read(self, arrayName, objectName='all'):
+        jsonArray = self.jsonData[arrayName]    # get only array
+        
+        if objectName == 'all':   # print all objects
+            return jsonArray
+                
+        
+        else:                      # print specific object
+            return jsonArray[objectName]
+    
+    def readAll(self):
+        return self.jsonData
+
+    def readObjectValue(self, arrayName, objectName, name):
+        jsonArray = self.jsonData[arrayName]
+        jsonObject = jsonArray[objectName]
+        objectValue = jsonObject[name]
+        return objectValue
+    
+
+    def update(self, arrayName, update):
+
+        # hard way to check if update is dict
+        updateIsDict = False
+        dictExample = {'aaa':'bbb'}
+
+        if type(update) == type(dictExample):
+            updateIsDict = True
+
+        assert updateIsDict, "'update' must be dictionary"
+
+
+        temp = self.jsonData[arrayName]
+        temp.append(update)
+
+        with open(self.fileName, 'w') as f:
+            json.dump(self.jsonData, f, indent=4)
+
+
+ezJson = EzJson('data.json')
